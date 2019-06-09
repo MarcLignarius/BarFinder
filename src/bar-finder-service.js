de2fbe2a1cd02c56d3cdbf5e6c2263ce60f4f6a4
Marc Davies <marcdaviesriot@gmail.com>
2019-06-08 23:34:01 -0700

    clean up code

--- src/bar-finder-service.js
+++ src/bar-finder-service.js
@@ -1,7 +1,6 @@
 export class BarFinder
 {
   findBar(city, state) {
-    // => this shit
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       let url = `https://api.openbrewerydb.org/breweries?by_city=${city}&by_state=${state}&sort=name`;
